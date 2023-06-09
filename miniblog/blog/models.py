9e368173f9ed241db718187949402d6fada67801
Ayush <ayushaggarwal42003@gmail.com>
2023-06-09 12:57:55 +0530

    add features

--- miniblog/blog/models.py
+++ miniblog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=200)
     desc=models.TextField()
+    timestamp=models.DateTimeField(default=timezone.now)
